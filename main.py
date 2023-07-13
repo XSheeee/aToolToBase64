2d7d7d263dee7e6de07731ff3401d71260a34f49
XShee <a1960995065@163.com>
2023-07-13 08:53:37 +0800

    v0.1t 先创建个窗口，麻麻喊我吃饭辣!

---
+++ main.py
@@ -0,0 +1,10 @@
+#项目开源地址：https://gitee.com/XShee/a-tool-to-base64
+import tkinter,base64
+def main():
+    #主窗口
+    mainWindow=tkinter.Tk()
+    #窗口标题
+    mainWindow.title("一个加密/解密 文字/图片的开源小工具")
+    #主窗口循环
+    mainWindow.mainloop()
+main()
